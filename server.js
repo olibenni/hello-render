86f08ed11173a370969903442b1bd25db1bef851
Olaf Magnússon <olafmagnus@asana.com>
2025-04-26 11:45:46 +0000

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Simple route
+app.get('/', (req, res) => {
+  res.send('Hello from Render!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
